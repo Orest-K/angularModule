72d1a2540d96c00c3b7771fb158921b0aa2a6fa2
Orest-K <Orest2558@gmail.com>
2020-07-11 21:54:03 +0300

    HW3 in progress

---
+++ HW3routerLinks/src/app/components/albums/alboum-previu/alboum-previu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alboum-previu',
+  templateUrl: './alboum-previu.component.html',
+  styleUrls: ['./alboum-previu.component.css']
+})
+export class AlboumPreviuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
